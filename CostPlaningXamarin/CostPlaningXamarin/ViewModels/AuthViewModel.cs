07f290287a398e03554ce966e0acbb30a6855e3d
vrucy <43879443+vrucy@users.noreply.github.com>
2021-03-02 11:16:26 +0100

    option to chose user who exist

--- CostPlaningXamarin/CostPlaningXamarin/ViewModels/AuthViewModel.cs
+++ CostPlaningXamarin/CostPlaningXamarin/ViewModels/AuthViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using CostPlaningXamarin.Command;
 using CostPlaningXamarin.Interfaces;
 using CostPlaningXamarin.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using System.Windows.Input;
 using Xamarin.Forms;
 
@@ -9,18 +11,98 @@ namespace CostPlaningXamarin.ViewModels
     public class AuthViewModel : BaseViewModel
     {
         ISQLiteService _sqliteService = DependencyService.Get<ISQLiteService>();
+        IWiFiManager _WiFiManager = DependencyService.Get<IWiFiManager>();
         INavigationServices _navigateService = DependencyService.Get<INavigationServices>();
         private ICommand _ApplyUser;
+        private ICommand _IsVisible;
+        private bool _isVisible;
+        private List<User> _users;
+        private User _selectedUser;
+
         public AuthViewModel()
         {
-            
+            _users = _sqliteService.GetUsers().GetAwaiter().GetResult();
+        }
+
+        private bool _isDiconnected;
+
+        public bool IsDiconnected
+        {
+            get { return _isDiconnected; }
+            set 
+            {
+                _isDiconnected = value;
+                if (!_WiFiManager.IsHomeWifiConnected() || !_WiFiManager.IsServerAvailable())
+                {
+                    _isDiconnected = false;
+                }
+                OnPropertyChanged("IsConnected");
+            }
+        }
+
+        public User SelectedUser
+        {
+            get { return _selectedUser; }
+            set
+            {
+                _selectedUser = value;
+                if (_selectedUser != null)
+                {
+                    var x = Application.Current.MainPage.DisplayAlert("Alert", string.Format("You chose {0} {1}. Are you sure?", _selectedUser.FirstName, _selectedUser.LastName), "Yes", "No");
+                    Task.Run(() =>
+                    {
+                        Task.Delay(500);
+                        ConfirmationUser(x.GetAwaiter().GetResult());
+
+                    });
+
+                }
+                OnPropertyChanged("SelectedUser");
+            }
+        }
+        private void ConfirmationUser(bool confirmation)
+        {
+            if (confirmation)
+            {
+                _sqliteService.CreateAppUser(_selectedUser);
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    _navigateService.NavigateToMainPage();
+                });
+            }
+            else
+            {
+                SelectedUser = null;
+                OnPropertyChanged("SelectedUser");
+            }
+
+        }
+
+        public List<User> Users
+        {
+            get { return _users; }
+            set { _users = value; OnPropertyChanged("Users"); }
+        }
+
+
+        public bool IsVisible
+        {
+            get
+            {
+                return _isVisible;
+            }
+            set
+            {
+                _isVisible = value;
+                OnPropertyChanged("IsVisible");
+            }
         }
         private User _user = new User();
 
         public User User
         {
             get { return _user; }
-            set 
+            set
             {
                 _user = value;
                 OnPropertyChanged("User");
@@ -42,6 +124,22 @@ namespace CostPlaningXamarin.ViewModels
         {
             _sqliteService.CreateAppUser(_user);
             _navigateService.NavigateToMainPage();
+        }
+        public ICommand IsVisibleCommand
+        {
+            get
+            {
+                if (_IsVisible == null)
+                {
+                    _IsVisible = new RelayCommand(IsVisibleAction);
+                }
+                return _IsVisible;
+            }
+        }
+        private void IsVisibleAction(object x)
+        {
+            _isVisible = true;
+            OnPropertyChanged("IsVisible");
         }
     }
 }
